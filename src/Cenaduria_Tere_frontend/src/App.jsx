331bb4606b3ae66223857c4e07b71b72f174a348
NintenCarlos <juancarlossantoyo0@gmail.com>
2024-06-05 19:41:35 -0600

    a.0.0.3

--- src/Cenaduria_Tere_frontend/src/App.jsx
+++ src/Cenaduria_Tere_frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Cenaduria_Tere_backend } from 'declarations/Cenaduria_Tere_backend';
-import Table from 'react-bootstrap/Table';
+import {Container, Row, Card, Table} from 'react-bootstrap';
 
 
 function App() {
@@ -18,35 +18,46 @@ function App() {
   };
 
   return (
-  <Table striped bordered hover>
-    <thead>
-      <tr>
-        <th>Número de reservación</th>
-        <th>Nombre del comensal</th>
-        <th>Número de asientos</th>
-        <th>Número de mesas</th>
-        <th>Fecha</th>
-        <th>Hora</th>
-      </tr>
-    </thead>
-    <tbody>
-      {
+  <Container className='m-2'>
+    <Row>
+      <Card>
+        <Card.Body>
+        <Card.Title>Cenaduría Doña Tere</Card.Title>  
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Número de reservación</th>
+              <th>Nombre del comensal</th>
+              <th>Número de asientos</th>
+              <th>Número de mesas</th>
+              <th>Fecha</th>
+              <th>Hora</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
 
-        reservs.length > 0?
-        reservs.map((rsv) => (
-          <tr>
-            <td>{rsv.numero}</td>
-            <td>{rsv.nombre}</td>
-            <td>{rsv.asientos}</td>
-            <td>{rsv.mesas}</td>
-            <td>*Fecha*</td>
-            <td>*Hora*</td>
-          </tr>
-        ))
-        :<tr></tr>
-      }
-    </tbody>
-  </Table>);
+              reservs.length > 0?
+              reservs.map((rsv) => (
+                <tr>
+                  <td>{rsv.numero}</td>
+                  <td>{rsv.nombre}</td>
+                  <td>{rsv.asientos}</td>
+                  <td>{rsv.mesas}</td>
+                  <td>*Fecha*</td>
+                  <td>*Hora*</td>
+                </tr>
+              ))
+              :<tr></tr>
+            }
+          </tbody>
+        </Table>
+        </Card.Body>
+        
+      </Card>
+    </Row>
+  </Container>
+  );
 }
 
 export default App;
